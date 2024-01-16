7965218aa4137a45ece4710b7d5c404042e4ecc6
Mykhailo Unniiiverse <unniiiverse@gmail.com>
2024-01-16 18:58:05 +0200

    1.0.5 | fix image scaling

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,8 +3,10 @@ import TestComponent from "./components/HashImage";
 
 const Home: React.FC = () => {
   return (
-    <div className="Home">
-      <TestComponent src="https://cdn4.cdn-telegram.org/file/LbChUE6IBYqK-VeVD8BofrtmaiPZXu5eQT7lFKCQ_Ua0PugS_aTzXhuVhitbV1cLW9hRpaktNZ9HVk702NYAc3ZxHv3Zjumlq6YW4EPjUmEPxRB-4IOha_om3wrGSqLORQMeB2IjxSdX3-CmDlco70_DHo1aSygBbPdZp2GZ6lGFnKItqaAYWcNUqfH56QnBAKoD5yV1dBjD7Mn3fzCqJObu2iDUuIUfxh6GMJK6sz2QZWoOQmDcCMa0giJPqvV98VgfsLCTbda4UkRX5ej2Ib4fQHCq3n_aPPsC71HUsskkPkk8ojVEfQtUqMXvliqwIJ91kBQnO4i3oXg4iPIAVA.jpg" alt="..." width={400} height={400} />
+    <div className="Home bg-red-400">
+      {/* <TestComponent src="https://cdn4.cdn-telegram.org/file/LbChUE6IBYqK-VeVD8BofrtmaiPZXu5eQT7lFKCQ_Ua0PugS_aTzXhuVhitbV1cLW9hRpaktNZ9HVk702NYAc3ZxHv3Zjumlq6YW4EPjUmEPxRB-4IOha_om3wrGSqLORQMeB2IjxSdX3-CmDlco70_DHo1aSygBbPdZp2GZ6lGFnKItqaAYWcNUqfH56QnBAKoD5yV1dBjD7Mn3fzCqJObu2iDUuIUfxh6GMJK6sz2QZWoOQmDcCMa0giJPqvV98VgfsLCTbda4UkRX5ej2Ib4fQHCq3n_aPPsC71HUsskkPkk8ojVEfQtUqMXvliqwIJ91kBQnO4i3oXg4iPIAVA.jpg" alt="..." width={400} height={400} /> */}
+      {/* <TestComponent src="https://cdn4.cdn-telegram.org/file/LbChUE6IBYqK-VeVD8BofrtmaiPZXu5eQT7lFKCQ_Ua0PugS_aTzXhuVhitbV1cLW9hRpaktNZ9HVk702NYAc3ZxHv3Zjumlq6YW4EPjUmEPxRB-4IOha_om3wrGSqLORQMeB2IjxSdX3-CmDlco70_DHo1aSygBbPdZp2GZ6lGFnKItqaAYWcNUqfH56QnBAKoD5yV1dBjD7Mn3fzCqJObu2iDUuIUfxh6GMJK6sz2QZWoOQmDcCMa0giJPqvV98VgfsLCTbda4UkRX5ej2Ib4fQHCq3n_aPPsC71HUsskkPkk8ojVEfQtUqMXvliqwIJ91kBQnO4i3oXg4iPIAVA.jpg" alt="..." fill parentClassName="!w-[600px] !h-[300px]" /> */}
+      {/* <TestComponent src="https://cdn4.cdn-telegram.org/file/LbChUE6IBYqK-VeVD8BofrtmaiPZXu5eQT7lFKCQ_Ua0PugS_aTzXhuVhitbV1cLW9hRpaktNZ9HVk702NYAc3ZxHv3Zjumlq6YW4EPjUmEPxRB-4IOha_om3wrGSqLORQMeB2IjxSdX3-CmDlco70_DHo1aSygBbPdZp2GZ6lGFnKItqaAYWcNUqfH56QnBAKoD5yV1dBjD7Mn3fzCqJObu2iDUuIUfxh6GMJK6sz2QZWoOQmDcCMa0giJPqvV98VgfsLCTbda4UkRX5ej2Ib4fQHCq3n_aPPsC71HUsskkPkk8ojVEfQtUqMXvliqwIJ91kBQnO4i3oXg4iPIAVA.jpg" alt="..." fill parentClassName="!w-[400px] !h-[600px]" /> */}
     </div>
   );
 }
--- src/components/HashImage.tsx
+++ src/components/HashImage.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import { Blurhash } from 'react-blurhash';
 
-import './styles.css'
+import './styles.scss'
 
 export interface HashImageProps extends React.DetailedHTMLProps<React.ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement> {
   src: string,
@@ -15,28 +15,51 @@ export interface HashImageProps extends React.DetailedHTMLProps<React.ImgHTMLAtt
   loading?: 'lazy' | 'eager',
 }
 
+// export const HashImage: React.FC<HashImageProps> = ({ src, alt, loading, initialHash, width, height, className, parentClassName, fill, ...props }) => {
+//   const [hash] = useState(initialHash || 'LRQ0XHWB?b%M~qofIURjWBt7j[M{');
+//   const [pictureLoaded, setPictureLoaded] = useState(false);
+//   const imgRef = useRef<HTMLImageElement>(null)
+
+//   useEffect(() => {
+//     if (!pictureLoaded || !imgRef.current) return;
+
+//     if (!fill) {
+//       if (!width || !height) {
+//         throw new Error('[hash-image]: In fill mode width and height are required')
+//       }
+//     }
+
+//     imgRef.current!.style.opacity = '100';
+//     imgRef.current!.style.visibility = 'visible';
+//   }, [pictureLoaded])
+
+//   return (
+//     <div className={`hash-image-parent ${parentClassName || ''}`} style={{ width: fill ? '100%' : width, height: fill ? '100%' : height }}>
+//       <Blurhash hash={hash} className="hash-image-hash" width={fill ? '100%' : width} height={fill ? '100%' : height} />
+//       <img src={src} alt={alt} ref={imgRef} loading={loading || 'lazy'} className={`hash-image-image ${className || ''}`} onLoad={() => setPictureLoaded(true)} {...props} />
+//     </div>
+//   );
+// }
+
 export const HashImage: React.FC<HashImageProps> = ({ src, alt, loading, initialHash, width, height, className, parentClassName, fill, ...props }) => {
   const [hash] = useState(initialHash || 'LRQ0XHWB?b%M~qofIURjWBt7j[M{');
   const [pictureLoaded, setPictureLoaded] = useState(false);
-  const imgRef = useRef<HTMLImageElement>(null)
+  const ref = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
-    if (!pictureLoaded || !imgRef.current) return;
+    if (pictureLoaded && ref.current) {
+      const image = ref.current.querySelector('img')!;
 
-    if (!fill) {
-      if (!width || !height) {
-        throw new Error('[hash-image]: In fill mode width and height are required')
-      }
+      image.classList.add('hash-image--loaded')
     }
-
-    imgRef.current!.style.opacity = '100';
-    imgRef.current!.style.visibility = 'visible';
-  }, [pictureLoaded])
+  }, [ref, pictureLoaded])
 
   return (
-    <div className={`hash-image-parent ${parentClassName || ''}`} style={{ width: fill ? '100%' : width, height: fill ? '100%' : height }}>
-      <Blurhash hash={hash} className="hash-image-hash" width={fill ? '100%' : width} height={fill ? '100%' : height} />
-      <img src={src} alt={alt} ref={imgRef} loading={loading || 'lazy'} className={`hash-image-image ${className || ''}`} onLoad={() => setPictureLoaded(true)} {...props} />
+    <div className={`hash-image-parent ${parentClassName ? parentClassName : ''}`} ref={ref} style={{ width: width || 'auto', height: height || 'auto' }}>
+      <picture className="hash-image-picture">
+        <Blurhash hash={hash} width={width || '100%'} height={height || '100%'} className="hash-image-placeholder" />
+        <img src={src} alt={alt} loading={loading || 'lazy'} {...props} className={`hash-image ${className ? className : ''}`} onLoad={() => setPictureLoaded(true)} />
+      </picture>
     </div>
   );
 }
