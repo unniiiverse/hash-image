998dea2addd334aac7151d28faab9d71f41e8135
Mykhailo Unniiiverse <unniiiverse@gmail.com>
2024-01-20 12:53:39 +0200

    1.1.0 | add flex-shrink 0 & remove bg in parent

--- src/components/HashImage.tsx
+++ src/components/HashImage.tsx
@@ -43,7 +43,7 @@ export const HashImage: React.FC<HashImageProps> = ({ src, alt, loading, initial
   }, [ref])
 
   return (
-    <div className={`hash-image-parent bg-red-200 ${parentClassName ? parentClassName : ''}`} ref={ref} style={{ width: width || 'auto', height: height || 'auto' }}>
+    <div className={`hash-image-parent ${parentClassName ? parentClassName : ''}`} ref={ref} style={{ width: width || 'auto', height: height || 'auto' }}>
       <picture className="hash-image-picture">
         <Blurhash hash={hash} width={width || '100%'} height={height || '100%'} className="hash-image-placeholder" />
         <img src={src} alt={alt} loading={loading || 'lazy'} {...props} className={`hash-image ${className ? className : ''}`} onLoad={() => setPictureLoaded(true)} />
